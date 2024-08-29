9d99d9e989bfba45cac8b09b15e4fb0315e5cab7
johnfernandis1999 <140940560+johnfernandis1999@users.noreply.github.com>
2024-08-29 22:46:28 +0530

    file stucture added

---
+++ src/pages/products/Products.jsx
@@ -0,0 +1,7 @@
+import "./products.scss";
+
+function Products() {
+  return <div className="products">Products</div>;
+}
+
+export default Products;
---
+++ src/pages/users/Users.jsx
@@ -0,0 +1,9 @@
+import "./users.scss";
+
+function Users() {
+  return (
+    <div className="users">Users</div>
+  )
+}
+
+export default Users;
